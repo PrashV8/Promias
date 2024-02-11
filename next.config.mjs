e3e6e3bf1649a138aaa692eb28e8702f3e7c4efa
PrashV8 <prashantvr8@gmail.com>
2024-02-11 18:09:21 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,52 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+      appDir: true,
+      serverComponentsExternalPackages: ["mongoose"],
+    },
+    images: {
+      domains: ['lh3.googleusercontent.com'],
+    },
+    webpack(config) {
+      config.experiments = {
+        ...config.experiments,
+        topLevelAwait: true,
+      }
+      return config
+    }
+  }
+  
+  export default nextConfig;
 
-export default nextConfig;
+/*
+
+
+
+
+
+
+/** @type {import('next').NextConfig} 
+const nextConfig = {
+    experimental: {
+      appDir: true,
+      serverComponentsExternalPackages: ["mongoose"],
+    },
+    images: {
+      domains: ['lh3.googleusercontent.com'],
+    },
+    webpack(config) {
+      config.experiments = {
+        ...config.experiments,
+        topLevelAwait: true,
+      }
+      return config
+    }
+  }
+  
+  module.exports = nextConfig
+
+
+
+
+
+*/
